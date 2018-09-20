3c18e1af2e074c75fdaa2739bd7275647813eb8d
Svein Ove Aas <sveina@gmail.com>
2018-09-20 14:39:49 +0100

    Configure znapzend for Saya

---
+++ modules/znapzend.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+  services.znapzend = {
+    enable = true;
+    autoCreation = true;
+    pure = true;
+    zetup = {
+      "rpool/home" = {
+        plan = "1d=>15m,1w=>1h";
+        recursive = true;
+        destinations.tsugumi = {
+          host = "znapzend@brage.info";
+          dataset = "stash/backups/${config.networking.hostName}/home";
+          plan = "1w=>1h,12w=>1d";
+        };
+      };
+    };
+  };
+}
+
