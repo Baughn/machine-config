e6a0427e0ea56f1486e0a3783cdc5039fe60530a
Svein Ove Aas <sveina@gmail.com>
2018-12-12 15:39:17 +0000

    Disable wayland on nVidia

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -4,4 +4,5 @@
     Option "metamodes" "nvidia-auto-select +0+0 { ForceCompositionPipeline = On }"
   '';
   environment.systemPackages = [ pkgs.nvtop ];
+  services.xserver.displayManager.gdm.wayland = false;
 }
