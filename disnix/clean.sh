d05f693f1cd02e70cefec41f5f9bc4918b5cddad
Svein Ove Aas <sveina@gmail.com>
2018-12-21 13:52:11 +0000

    Remove the kubernetes/disnix detritus

--- disnix/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env nix-shell
-#!nix-shell -i bash -p bash disnix
-
-set -eu
-
-cd "$(readlink -f "$(dirname "$0")")"
-
-disnix-collect-garbage -d production/infrastructure.nix
--- disnix/third_party/mediawiki-1.30.0/resources/src/mediawiki.special/mediawiki.special.preferences.convertmessagebox.js
+++
@@ -1,9 +0,0 @@
-/*!
- * JavaScript for Special:Preferences: Check for successbox to replace with notifications.
- */
-( function ( mw, $ ) {
-	$( function () {
-		var convertmessagebox = require( 'mediawiki.notification.convertmessagebox' );
-		convertmessagebox();
-	} );
-}( mediaWiki, jQuery ) );
