5c393315431d5d4f5f4bb73ba8c080a8f3125cb0
Svein Ove Aas <sveina@gmail.com>
2019-01-07 17:02:54 +0000

    madoka: tank -> rpool

--- madoka/minecraft.nix
+++ madoka/minecraft.nix
@@ -5,9 +5,9 @@
     minecraft ALL= NOPASSWD: /run/current-system/sw/bin/zpool status*
     minecraft ALL= NOPASSWD: /run/current-system/sw/bin/zpool list*
     minecraft ALL= NOPASSWD: /run/current-system/sw/bin/zfs list*
-    minecraft ALL= NOPASSWD: /run/current-system/sw/bin/zfs snapshot tank/home/minecraft*
-    minecraft ALL= NOPASSWD: /run/current-system/sw/bin/zfs rollback tank/home/minecraft*
-    minecraft ALL= NOPASSWD: /run/current-system/sw/bin/mount -t zfs --target /home/minecraft/snapshot --source tank/home/minecraft*
+    minecraft ALL= NOPASSWD: /run/current-system/sw/bin/zfs snapshot rpool/home/minecraft*
+    minecraft ALL= NOPASSWD: /run/current-system/sw/bin/zfs rollback rpool/home/minecraft*
+    minecraft ALL= NOPASSWD: /run/current-system/sw/bin/mount -t zfs --target /home/minecraft/snapshot --source rpool/home/minecraft*
     minecraft ALL= NOPASSWD: /run/current-system/sw/bin/umount /home/minecraft/snapshot
   '';
 
