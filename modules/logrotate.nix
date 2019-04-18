edfbe2ef261931bcda33e3cac193f0c9105fd78e
Svein Ove Aas <sveina@gmail.com>
2019-04-18 22:28:40 +0100

    Fix logrotate

--- modules/logrotate.nix
+++ modules/logrotate.nix
@@ -3,11 +3,12 @@
   services.logrotate.enable = true;
   services.logrotate.config = ''
     compress
+    compresscmd ${pkgs.zstd}/bin/zstd
+    compressext zst
 
-    /var/spool/nginx/logs {
+    /var/spool/nginx/logs/*.log {
       rotate 5
       weekly
-      size 100M
       postrotate
         ${pkgs.psmisc}/bin/killall -USR1 nginx
       endscript
