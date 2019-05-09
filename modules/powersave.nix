3d8cf9d52463a98b10f5cc9176fcf8f2e9621363
Svein Ove Aas <sveina@gmail.com>
2019-05-09 21:56:17 +0100

    tlp: Always use battery mode

--- modules/powersave.nix
+++ modules/powersave.nix
@@ -2,7 +2,12 @@
 
 {
   # Power saving settings.
-  services.tlp.enable = true;
+  services.tlp = {
+    enable = true;
+    extraConfig = ''
+      SOUND_POWER_SAVE_ON_AC=1
+    '';
+  };
   services.upower.enable = true;
 
   powerManagement = {
