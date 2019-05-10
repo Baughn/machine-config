a916cf18ab2f9c88f0b787117e85d85946a11e17
Svein Ove Aas <sveina@gmail.com>
2019-05-10 20:36:11 +0100

    kaho: More TLP tuning

--- modules/powersave.nix
+++ modules/powersave.nix
@@ -5,7 +5,14 @@
   services.tlp = {
     enable = true;
     extraConfig = ''
+      # For testing, let's make sure we get the experience.
+      TLP_DEFAULT_MODE=BAT
+      TLP_PERSISTENT_DEFAULT=1
+
       SOUND_POWER_SAVE_ON_AC=1
+      CPU_MAX_PERF_ON_BAT=75
+      WIFI_PWR_ON_AC=on
+      DEVICES_TO_DISABLE_ON_STARTUP="bluetooth wwan"
     '';
   };
   services.upower.enable = true;
