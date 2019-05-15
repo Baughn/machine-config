76bc03cab288f33e10511163329d71a20b83b0e0
Svein Ove Aas <sveina@gmail.com>
2019-05-15 21:38:55 +0100

    More kaho-specced installer changes

--- installer/cd.nix
+++ installer/cd.nix
@@ -9,7 +9,12 @@
   networking.hostId = "12345678";
 
   # For the Dell.
+  boot.loader.systemd-boot.consoleMode = "0";
   i18n.consoleFont = "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
   boot.zfs.enableUnstable = true;
   boot.zfs.requestEncryptionCredentials = true;
+
+  environment.etc.kaho-system.source = (import <nixpkgs/nixos> {
+    configuration = ../kaho;
+  }).system;
 }
