796dfdaf2f44756ad54cfb982e85046400e2eb52
Svein Ove Aas <sveina@gmail.com>
2019-06-02 22:32:23 +0100

    Yet more power saving configs

--- modules/powersave.nix
+++ modules/powersave.nix
@@ -10,11 +10,16 @@
       TLP_PERSISTENT_DEFAULT=1
 
       SOUND_POWER_SAVE_ON_AC=1
+      WIFI_PWR_ON_AC=on
+      DEVICES_TO_DISABLE_ON_STARTUP="bluetooth wwan"
+
+      CPU_SCALING_GOVERNOR_ON_BAT=powersave
+      CPU_SCALING_GOVERNOR_ON_AC=powersave
       CPU_MAX_PERF_ON_BAT=100
       CPU_SCALING_MAX_FREQ_ON_BAT=3000000
       CPU_BOOST_ON_BAT=1
-      WIFI_PWR_ON_AC=on
-      DEVICES_TO_DISABLE_ON_STARTUP="bluetooth wwan"
+
+      AHCI_RUNTIME_PM_ON_BAT=auto
     '';
   };
   services.upower.enable = true;
