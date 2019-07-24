946b406bc52fadc6d74d86d9d23fbf4dfb5f8c17
Svein Ove Aas <sveina@gmail.com>
2019-07-24 15:26:28 +0100

    Misc

--- kaho/default.nix
+++ kaho/default.nix
@@ -68,11 +68,4 @@
   # Enable touchpad support.
   #services.xserver.libinput.enable = true;
   services.xserver.synaptics.enable = true;
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "19.03"; # Did you read the comment?
-
 }
