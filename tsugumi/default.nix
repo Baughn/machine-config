37a929948dddd42b9b7199746165cb1772814a79
Svein Ove Aas <sveina@gmail.com>
2019-08-16 17:14:37 +0100

    Not using znapzend, so remove related workarounds

--- tsugumi/default.nix
+++ tsugumi/default.nix
@@ -41,11 +41,6 @@
     device = "nodev";
   };
 
-  # Work around https://github.com/oetiker/znapzend/issues/376
-  services.openssh.extraConfig = ''
-    MaxStartups 1000
-  '';
-  
   # Power management:
   #services.thermald.enable = true;
   powerManagement.enable = true;
