42c4b7e9b4e67b56447d09c588a35bae151869c1
Svein Ove Aas <sveina@gmail.com>
2021-03-01 21:02:19 +0000

    Switch from nginx to caddy (take 2)

--- tsugumi/default.nix
+++ tsugumi/default.nix
@@ -168,119 +168,71 @@
   # ZNC
   #services.znc.enable = true;
 
-  # Webserver (ACME, nginx)
-  security.acme = {
-    acceptTerms = true;
-    certs = {
-      "brage.info" = {
-        email = "sveina@gmail.com";
-        group = "nginx";
-        postRun = "systemctl reload nginx.service";
-        webroot = "/var/lib/acme/acme-challenge";
-        extraDomains = {
-          "madoka.brage.info" = null;
-          "status.brage.info" = null;
-          "grafana.brage.info" = null;
-#          "tppi.brage.info" = null;
-#          "alertmanager.brage.info" = null;
-          "map.brage.info" = null;
-          "incognito.brage.info" = null;
-#          "tppi-map.brage.info" = null;
-#          "cache.brage.info" = null;
-          "znc.brage.info" = null;
-#          "quest.brage.info" = null;
-          "warmroast.brage.info" = null;
-#          "hydra.brage.info" = null;
-#          "pw.brage.info" = null;
-#          "ll.ja13.org" = null;
-#          "ctl.ll.ja13.org" = null;
-        };
-      };
-    };
-  };
-
-  systemd.services.nginx = {
-    serviceConfig.ProtectHome = false;
-  };
-  services.nginx = let 
-    headers = ''
-      add_header Strict-Transport-Security "max-age=31536000; includeSubdomains";
-      add_header X-Clacks-Overhead "GNU Terry Pratchett";
-      add_header X-Frame-Options "allow-from https://madoka.brage.info";
-      add_header X-XSS-Protection "1; mode=block";
-      add_header X-Content-Type-Options "nosniff";
-      add_header Referrer-Policy "no-referrer-when-downgrade";
-      add_header Content-Security-Policy-Report-Only "default-src 'self'; report-uri /__cspreport__;";
-
-      limit_rate 3750000;
-    '';
-    base = x: {
-      forceSSL = true;
-      useACMEHost = "brage.info";
-    } // x;
-    proxy = port: base {
-      locations."/".proxyPass = "http://127.0.0.1:" + toString(port) + "/";
-    };
-    proxyJared = port: base {
-      locations."/".proxyPass = "http://10.211.72.90:" + toString(port) + "/";
-    };
-  in {
+  # Webserver (Caddy)
+  systemd.services.caddy.serviceConfig.ProtectHome = false;
+  services.caddy = {
     enable = true;
-    recommendedGzipSettings = true;
-    recommendedOptimisation = true;
-    recommendedProxySettings = true;
-    recommendedTlsSettings = true;
-    sslDhparam = ./nginx/dhparams.pem;
-    sslProtocols = "TLSv1.2 TLSv1.3";
-    sslCiphers = "ALL:EECDH+aRSA+AESGCM:EDH+aRSA:EECDH+aRSA:+AES256:+AES128:+SHA1:!CAMELLIA:!SEED:!3DES:!DES:!RC4:!eNULL";
-    appendHttpConfig = ''
-      ${headers}
-      etag on;
+    email = "sveina@gmail.com";
+    config = ''
+      (headers) {
+        header Strict-Transport-Security "max-age=31536000; includeSubdomains"
+        header X-Clacks-Overhead "GNU Terry Pratchett"
+        header X-Frame-Options "allow-from https://madoka.brage.info"
+        header X-XSS-Protection "1; mode=block"
+        header X-Content-Type-Options "nosniff"
+        header Referrer-Policy "no-referrer-when-downgrade"
+        encode zstd gzip
+        handle_errors {
+          header content-type "text/plain"
+          respond "{http.error.status_code} {http.error.status_text}"
+        }
+      }
+
+      madoka.brage.info {
+        root * /home/minecraft/web/
+        import headers
+        reverse_proxy /warmroast/* localhost:23000
+        file_server browse
+      }
+
+      map.brage.info {
+        import headers
+        reverse_proxy http://127.0.0.1:8123
+      }
+
+      incognito.brage.info {
+        import headers
+        reverse_proxy http://127.0.0.1:8124
+      }
+
+      status.brage.info {
+        import headers
+        reverse_proxy http://127.0.0.1:9090
+      }
+
+      znc.brage.info {
+        import headers
+        reverse_proxy https://znc.brage.info:4000 {
+        }
+      }
+
+      ar-innna.brage.info {
+        root * /home/aquagon/web/
+        import headers
+        file_server browse
+      }
+
+      brage.info {
+        root * /home/svein/web/
+        import headers
+        file_server browse
+      }
+
+      www.brage.info, tsugumi.brage.info {
+        import headers
+        redir https://brage.info/
+      }
     '';
-    virtualHosts = {
-      "madoka.brage.info" = base {
-        locations = {
-          "/" = {
-            root = "/home/minecraft/web";
-            tryFiles = "\$uri \$uri/ =404";
-            extraConfig = ''
-              add_header Cache-Control "public";
-              disable_symlinks off;
-              autoindex on;
-              ${headers}
-              expires 1h;
-            '';
-          };
-          "/warmroast/".proxyPass = "http://127.0.0.1:23000/";
-          "/baughn/".extraConfig = "alias /home/svein/web/;";
-          "/tppi/".extraConfig = "alias /home/tppi/web/;";
-        };
-      };
-      "map.brage.info" = proxy 8123;
-      "incognito.brage.info" = proxy 8124;
-      "status.brage.info" = proxy 9090;
-      "znc.brage.info" = base {
-         locations."/" = {
-           proxyPass = "https://127.0.0.1:4000";
-           extraConfig = "proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;";
-         };
-      };
-      "brage.info" = base {
-        default = true;
-        serverAliases = [ "tsugumi.brage.info" "www.brage.info" ];
-        locations."/".root = "/home/svein/web/";
-        extraConfig = "autoindex on;";
-      };
-      "ar-innna.brage.info" = base {
-        locations."/" = {
-          root = "/home/aquagon/web/";
-          extraConfig = "autoindex on;";
-        };
-      };
-      "wiki.sufficientvelocity.com" = base {
-        locations."/".proxyPass = "http://127.0.0.1:3300";
-      };
-    };
   };
 
   users.include = ["pl" "aquagon" "will" "snowfire" "minecraft" "linuxgsm"];
