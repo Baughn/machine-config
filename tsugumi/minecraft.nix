ee950fdab7378e6d14bf8383613745eb64659ec5
Svein Ove Aas <sveina@gmail.com>
2021-04-01 20:16:04 +0100

    Re-enable minecraft

--- tsugumi/minecraft.nix
+++ tsugumi/minecraft.nix
@@ -5,9 +5,9 @@
     minecraft ALL= NOPASSWD: /run/current-system/sw/bin/zpool status*
     minecraft ALL= NOPASSWD: /run/current-system/sw/bin/zpool list*
     minecraft ALL= NOPASSWD: /run/current-system/sw/bin/zfs list*
-    minecraft ALL= NOPASSWD: /run/current-system/sw/bin/zfs snapshot minecraft*
-    minecraft ALL= NOPASSWD: /run/current-system/sw/bin/zfs rollback minecraft*
-    minecraft ALL= NOPASSWD: /run/current-system/sw/bin/mount -t zfs --target /home/minecraft/snapshot --source minecraft*
+    minecraft ALL= NOPASSWD: /run/current-system/sw/bin/zfs snapshot rpool/minecraft*
+    minecraft ALL= NOPASSWD: /run/current-system/sw/bin/zfs rollback rpool/minecraft*
+    minecraft ALL= NOPASSWD: /run/current-system/sw/bin/mount -t zfs --target /home/minecraft/snapshot --source rpool/minecraft*
     minecraft ALL= NOPASSWD: /run/current-system/sw/bin/umount /home/minecraft/snapshot
   '';
 }
