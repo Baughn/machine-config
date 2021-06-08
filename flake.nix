ee02d5dfa9bfb69128381213f0152a6c2c5a9260
Svein Ove Aas <sveina@gmail.com>
2021-06-08 19:10:54 +0100

    Initial flakification of tsugumi

--- flake.nix
+++ flake.nix
@@ -3,13 +3,24 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.hercules-ci-agent.url = "github:hercules-ci/hercules-ci-agent/nixos-unstable";
-
-  outputs = { self, nixpkgs, hercules-ci-agent }: {
-    nixosConfigurations.tsugumi = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./tsugumi/configuration.nix
-      ];
-    };
+  inputs.openwrt = {
+    url = "path:../openwrt";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
+
+  outputs = { self, nixpkgs, hercules-ci-agent, openwrt }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell.${system} = import ./shell.nix { inherit pkgs; };
+      nixosConfigurations.tsugumi = nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          openwrt.nixosModule
+          ./tsugumi/configuration.nix
+        ];
+      };
+    };
 }
