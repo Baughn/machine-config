8334c3be734a3f360f23195ca1a692506d6fc053
Svein Ove Aas <svein@google.com>
2022-01-02 23:07:01 +0000

    Implement deploy-rs

--- flake.nix
+++ flake.nix
@@ -5,12 +5,14 @@
   inputs.nixos-hardware.url = "flake:nixos-hardware";
   inputs.home-manager.url = "github:nix-community/home-manager";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.deploy-rs.url = "github:serokell/deploy-rs";
+
   #inputs.openwrt = {
   #  url = "path:../openwrt";
   #  inputs.nixpkgs.follows = "nixpkgs";
   #};
 
-  outputs = { self, nixpkgs, nixos-hardware, home-manager }:
+  outputs = { self, nixpkgs, nixos-hardware, home-manager, deploy-rs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -27,8 +29,18 @@
             };
           }
       ];
+      deployNodes = hosts: pkgs.lib.listToAttrs (map (host: pkgs.lib.nameValuePair host
+      {
+        hostname = "${host}.brage.info";
+        profiles.system = {
+          user = "root";
+          path = deploy-rs.lib.${system}.activate.nixos self.nixosConfigurations.${host};
+        };
+      }) hosts);
     in {
       devShell.${system} = import ./shell.nix { inherit pkgs; };
+
+      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
 
       packages.${system} = {
         install-cd = (installer [
@@ -41,14 +53,16 @@
         ]).config.system.build.kexec_tarball;
       };
 
+      deploy.nodes = deployNodes [ "tromso" "saya" "tsugumi" ];
+
       nixosConfigurations.saya = nixpkgs.lib.nixosSystem {
         inherit system;
 
-	modules = [
+        modules = [
           nixos-hardware.nixosModules.common-pc
           nixos-hardware.nixosModules.common-cpu-amd
-	  ./saya/configuration.nix
-	] ++ homeConfig;
+    ./saya/configuration.nix
+  ] ++ homeConfig;
       };
 
       nixosConfigurations.tsugumi = nixpkgs.lib.nixosSystem {
@@ -66,12 +80,12 @@
       nixosConfigurations.tromso = nixpkgs.lib.nixosSystem {
         inherit system;
 
-	modules = [
-	  nixos-hardware.nixosModules.common-pc
-	  nixos-hardware.nixosModules.common-cpu-amd
-	  nixos-hardware.nixosModules.common-gpu-amd
-	  ./tromso/configuration.nix
-	] ++ homeConfig;
+        modules = [
+          nixos-hardware.nixosModules.common-pc
+          nixos-hardware.nixosModules.common-cpu-amd
+          nixos-hardware.nixosModules.common-gpu-amd
+          ./tromso/configuration.nix
+        ] ++ homeConfig;
       };
     };
 }
