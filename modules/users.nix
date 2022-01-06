dee5b176ae5216d62e41d4888a7fb873749a410f
Svein Ove Aas <svein@google.com>
2022-01-06 18:36:48 +0000

    Misc. secret updates & re-enable initrd ssh

--- modules/users.nix
+++ modules/users.nix
@@ -6,7 +6,7 @@ let
     svein = {
       uid = 1000;
       extraGroups = [ "wheel" "wireshark" "systemd-journal" "disnix" "networkmanager" ];
-      inherit (import ../secrets) initialPassword;
+      passwordFile = config.age.secrets.userPassword.path;
       createHome = false;
     };
     bloxgate.uid = 1001;
