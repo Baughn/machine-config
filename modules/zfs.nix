04793e8674b4b5598f9f0736603bc7ef3fd89767
Svein Ove Aas <svein@google.com>
2022-01-06 15:28:58 +0000

    Fix zfs setup

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./zrepl.nix
@@ -14,13 +14,13 @@
     echo 300 > /sys/module/zfs/parameters/zfs_txg_timeout
 
     for i in /dev/sd? ; do
-      if smartctl -l scterc,70,70 $i > /dev/null ; then
+      if ${pkgs.smartmontools}/bin/smartctl -l scterc,70,70 $i > /dev/null ; then
           echo -n $i " is good "
       else
           echo 180 > /sys/block/''${i/\/dev\/}/device/timeout
           echo -n $i " is  bad "
       fi;
-      smartctl -i $i | egrep "(Device Model|Product:)"
+      ${pkgs.smartmontools}/bin/smartctl -i $i | ${pkgs.gnugrep}/bin/egrep "(Device Model|Product:)"
       blockdev --setra 1024 $i
     done
   '';
