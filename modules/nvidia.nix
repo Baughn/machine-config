faab40a42ff5c0e1284fe9a566d53955bcf0994f
Svein Ove Aas <sveina@gmail.com>
2022-02-11 15:12:52 +0000

    Misc

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -2,7 +2,7 @@
   hardware.nvidia.modesetting.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   services.xserver.screenSection = ''
-    Option "metamodes" "nvidia-auto-select +0+0 { ForceCompositionPipeline = On }"
+    Option "metamodes" "nvidia-auto-select +0+0 { ForceCompositionPipeline = On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On }"
   '';
   environment.systemPackages = [ pkgs.nvtop ];
 #  services.xserver.displayManager.gdm.wayland = false;
