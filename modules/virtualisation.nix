a8e76917dc8ee6edb063a868861cadc04eced0d2
Svein Ove Aas <sveina@gmail.com>
2022-02-12 00:34:38 +0000

    Disable virtd and lxd, I never use them

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -7,12 +7,12 @@
   };
 
   config = lib.mkIf config.me.virtualisation.enable {
-    virtualisation.libvirtd.enable = true;
-    virtualisation.lxd.enable = true;
+    virtualisation.libvirtd.enable = false;
+    virtualisation.lxd.enable = false;
     virtualisation.docker.enable = true;
     virtualisation.docker.storageDriver = "zfs";
     users.extraUsers.svein.extraGroups = [ "docker" "lxd" "libvirtd" ];
     networking.firewall.checkReversePath = false;
-    environment.systemPackages = [ pkgs.qemu pkgs.virtmanager pkgs.nixos-shell ];
+    environment.systemPackages = [ pkgs.qemu pkgs.nixos-shell ];
   };
 }
