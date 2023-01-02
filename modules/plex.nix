7934ea28473c6636112aacb38f138d2546687d23
Svein Ove Aas <sveina@gmail.com>
2023-01-02 13:52:31 +0000

    plex: Expose

--- modules/plex.nix
+++ modules/plex.nix
@@ -5,6 +5,9 @@
 }: {
   services.plex.enable = true;
   services.plex.openFirewall = false;
+  networking.firewall.interfaces.external = {
+    allowedTCPPorts = [ 32400 ];
+  };
   networking.firewall.interfaces.internal = {
     allowedTCPPorts = [32400 3005 8324 32469];
     allowedUDPPorts = [1900 5353 32410 32412 32413 32414];
