4926e6e906ecd8ba09749a3ed2342491d7f78f10
Svein Ove Aas <sveina@gmail.com>
2023-04-29 18:51:45 +0100

    Bump nixpkgs and stuff

--- modules/plex.nix
+++ modules/plex.nix
@@ -8,8 +8,8 @@
   networking.firewall.interfaces.external = {
     allowedTCPPorts = [ 32400 ];
   };
-  networking.firewall.interfaces.internal = {
-    allowedTCPPorts = [32400 3005 8324 32469];
-    allowedUDPPorts = [1900 5353 32410 32412 32413 32414];
-  };
+#  networking.firewall.interfaces.internal = {
+#    allowedTCPPorts = [32400 3005 8324 32469];
+#    allowedUDPPorts = [1900 5353 32410 32412 32413 32414];
+#  };
 }
