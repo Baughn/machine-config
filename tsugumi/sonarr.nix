3cd47f9da1299054818626dfa545c7ea136763cf
Svein Ove Aas <sveina@gmail.com>
2023-07-15 23:20:00 +0100

    Run qbittorrent

--- tsugumi/sonarr.nix
+++ tsugumi/sonarr.nix
@@ -1,6 +1,20 @@
+{ pkgs, ... }:
+
 {
   services.bazarr.enable = true;
   services.sonarr.enable = true;
   services.prowlarr.enable = true;
   services.jellyfin.enable = true;
+
+  # Also run qbittorrent-nox as svein.
+  systemd.services.qbittorrent-sonarr = {
+    description = "qbittorrent";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    script = ''
+      ${pkgs.qbittorrent-nox}/bin/qbittorrent-nox
+    '';
+    serviceConfig.User = "svein";
+  };
 }
+
