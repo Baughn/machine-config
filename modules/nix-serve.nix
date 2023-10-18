d18969b179b002ca3a2e3742d124640b72c4053b
Svein Ove Aas <sveina@gmail.com>
2023-10-18 16:52:55 +0100

    nix-store fixes

--- modules/nix-serve.nix
+++ modules/nix-serve.nix
@@ -8,12 +8,12 @@ lib.mkIf (config.networking.hostName == "tsugumi" || config.networking.hostName 
     enable = true;
     port = 5000;
     secretKeyFile = config.age.secrets."nix-store/private-key".path;
+    openFirewall = true;
   };
 
   # And add whichever machine we *aren't* on as a binary cache.
   nix.settings = {
     substituters = lib.mkBefore [(if config.networking.hostName == "tsugumi" then "http://saya:5000" else "http://tsugumi:5000")];
+    trusted-public-keys = [ (builtins.readFile ../secrets/nix-store/public-key) ];
   };
-
-  networking.firewall.allowedTCPPorts = [ 5000 ];
 }
