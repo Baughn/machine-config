013753e2408f179b8a54b9a5ef87dcf75d499e98
Svein Ove Aas <sveina@gmail.com>
2024-02-08 16:13:53 +0000

    Reconfigure bcachefs module

--- modules/bcachefs.nix
+++ modules/bcachefs.nix
@@ -1,33 +1,24 @@
 { lib, pkgs, ... }:
 
 {
-  boot.supportedFilesystems = ["bcachefs"];
-  boot.initrd.supportedFilesystems = ["bcachefs"];
-
-  # Makes `availableOn` fail for zfs, see <nixos/modules/profiles/base.nix>.
-  # This is a workaround since we cannot remove the `"zfs"` string from `supportedFilesystems`.
-  # The proper fix would be to make `supportedFilesystems` an attrset with true/false which we
-  # could then `lib.mkForce false`
-  nixpkgs.overlays = [(final: super: {
-    zfs = super.zfs.overrideAttrs(_: {
-      meta.platforms = [];
-    });
-  })];
+  # Useful flags: BCACHEFS_DEBUG y
 
   boot.kernelPatches = [ {
-      name = "bcachefs-config";
-      patch = null;
-      extraConfig = ''
-        PREEMPT_VOLUNTARY n
-        PREEMPT y
-        BCACHEFS_DEBUG y
-        KALLSYMS y
-        KALLSYMS_ALL y
-        DEBUG_FS y
-        DYNAMIC_FTRACE y
-        FTRACE y
-      '';
-  } ];
+    name = "bcachefs-config";
+    patch = null;
+    extraConfig = ''
+      PREEMPT_VOLUNTARY n
+      PREEMPT y
+      KALLSYMS y
+      KALLSYMS_ALL y
+      DEBUG_FS y
+      DYNAMIC_FTRACE y
+      FTRACE y
+      TASK_DELAY_ACCT y
+    '';
+  }];
 
+  boot.supportedFilesystems = ["bcachefs"];
+  boot.initrd.supportedFilesystems = ["bcachefs"];
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
 }
