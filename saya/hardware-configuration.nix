3f8f68861234bbd7057a0da4b00c13d86a1b6295
Svein Ove Aas <sveina@gmail.com>
2024-02-21 18:03:35 +0000

    Reconfigure saya

--- saya/hardware-configuration.nix
+++ saya/hardware-configuration.nix
@@ -10,7 +10,7 @@
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "uas" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
@@ -48,6 +48,7 @@
     { device = "/home/svein/web";
       depends = [ "/home" ];
       options = [ "bind" ];
+    };
 
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/48DE-02A2";
