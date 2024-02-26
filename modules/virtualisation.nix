93fd189dc862f6d91cfb4c91c2a5464ed19ef7a2
Svein Ove Aas <sveina@gmail.com>
2024-02-26 15:18:09 +0000

    Bump nixpkgs

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -15,14 +15,6 @@
     virtualisation.docker.enable = true;
     users.extraUsers.svein.extraGroups = ["docker" "lxd" "libvirtd"];
     networking.firewall.checkReversePath = false;
-    virtualisation.podman = {
-      enable = true;
-      enableNvidia = true;
-      autoPrune.enable = true;
-      defaultNetwork.settings = {
-        dns_enabled = true;
-      };
-    };
     environment.systemPackages = [
       pkgs.qemu
       pkgs.nixos-shell
