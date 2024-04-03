efcfa66ef4479f252d7a05956e66ec2822725cc0
Svein Ove Aas <sveina@gmail.com>
2024-04-03 18:59:46 +0100

    Fix jared's unfortunate typo

--- modules/keys.nix
+++ modules/keys.nix
@@ -83,11 +83,11 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBpUHboM5fNLogflF/9EEklCgAvmE08L1lmT696UIwSW jared@arch01l.ctha.ja4.org"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG5nvmj3DlRnRJqsvdizUOMcQH71XQxew5jI6WW65Gpv jared@win01d.ctha.ja4.org"
     ];
-    wireguard = [
+    wireguard = [{
       # Tunnel from jump.use1.ja4.org
       publicKey = "3dw3YKuBXdSQj/ULDM9mj1VKotWNEWSNVW6FcIIsR2A=";
       allowedIPs = ["35.168.203.255/32"];
-    ];
+    }];
   };
   maxwell-lt = {
     ssh = [
