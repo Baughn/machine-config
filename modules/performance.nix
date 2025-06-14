4020ce4024a4e2302d02bfea36dc1854aa27f220
Svein Ove Aas <sveina@gmail.com>
2025-06-14 23:17:50 +0100

    Enable rtkit/ananicy

---
+++ modules/performance.nix
@@ -0,0 +1,5 @@
+{
+  # Allow things that need real-time (like sound) to get real-time.
+  security.rtkit.enable = true;
+  services.ananicy.enable = true;
+}
--- saya/configuration.nix
+++ saya/configuration.nix
@@ -9,7 +9,7 @@
     [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../modules/zsh.nix
+      ../modules
     ];
 
   # Boot
