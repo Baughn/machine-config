009934be4eeec3184ab11f85859d11d614d1ea70
Svein Ove Aas <sveina@gmail.com>
2025-07-23 21:31:14 +0100

    Use ~/dev/nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Machine configurations for all my machines";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "git+file:///home/svein/dev/nixpkgs";
     nixpkgs-kernel.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     nix-index-database.url = "github:nix-community/nix-index-database";
