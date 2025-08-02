4fa9b284b51677f072982b6267da99245cdfc82d
Svein Ove Aas <sveina@gmail.com>
2025-08-02 17:31:27 +0200

    fix(pull.sh): Let it run without ping-discord

--- pull.sh
+++ pull.sh
@@ -4,5 +4,9 @@ set -euo pipefail
 cd "$(dirname "$(readlink -f "$0")")"
 
 jj git fetch
-ping-discord update -m 'Pulled changes:' -p '*' -r 'master..master@origin'
+if command -v ping-discord > /dev/null; then
+  ping-discord update -m 'Pulled changes:' -p '*' -r 'master..master@origin'
+else
+  echo 'No ping-discord here (skipped)'
+fi
 jj rebase -r master..@ -d master@origin
