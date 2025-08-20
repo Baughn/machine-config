d834cd2e1bec4255a9e339dd5955c989f9ead001
Svein Ove Aas <sveina@gmail.com>
2025-08-20 12:45:43 +0100

    Coalesce bumps

--- push.sh
+++ push.sh
@@ -6,5 +6,10 @@ cd "$(dirname "$(readlink -f "$0")")"
 # Check for minecraft-related changes
 ping-discord update --patterns "*minecraft*,*ssh_host_*,modules/users.nix,modules/keys.nix,push.sh,tools/ping-discord/*" || true
 
+jj squash \
+  --from 'reachable(latest(Bumps), Bumps)' \
+  --to 'latest(Bumps)' \
+  --use-destination-message \
+  || true
 jj bookmark set master -r 'latest(ancestors(@-) & ~empty() & ~description(exact:""))'
 jj git push
