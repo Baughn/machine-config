d0245cca8410e2b1ead0837490c84d6d91b0242c
Svein Ove Aas <sveina@gmail.com>
2026-03-23 22:43:43 +0000

    Rebuild: Support flags

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p nix-output-monitor
+
 set -euo pipefail
 
-sudo nixos-rebuild switch --log-format internal-json "$@" |& nom --json
+MODE=${1:-switch}
+
+sudo nixos-rebuild $MODE --log-format internal-json |& nom --json
