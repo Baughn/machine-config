dfb2e6b66e013fd9bae30d25d8a7b780939d0d51
Svein Ove Aas <sveina@gmail.com>
2026-04-12 11:44:41 +0100

    Disable firejail for firefox; it keeps breaking the profile

--- modules/firejail/nixos.nix
+++ modules/firejail/nixos.nix
@@ -15,10 +15,6 @@ in
     programs.firejail = {
       enable = true;
       wrappedBinaries = {
-        firefox = {
-          executable = "${pkgs.firefox}/bin/firefox";
-          profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
-        };
         discord = {
           executable = "${pkgs.discord}/bin/discord";
           profile = "${pkgs.firejail}/etc/firejail/discord.profile";
