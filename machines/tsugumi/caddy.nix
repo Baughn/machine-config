d823a9b5caf72bb3368161999268fc6ea8610784
Svein Ove Aas <sveina@gmail.com>
2026-07-03 14:01:54 +0100

    Update Caddy-dns version & token

--- machines/tsugumi/caddy.nix
+++ machines/tsugumi/caddy.nix
@@ -4,8 +4,8 @@
   services.caddy = {
     enable = true;
     package = pkgs.caddy.withPlugins {
-      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
-      hash = "sha256-I0FjQOfFaGlOEJlQECmYNBKjIY4CIg5aCCQ/ORmnrSU=";
+      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
+      hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg";
     };
     email = "sveina@gmail.com";
     environmentFile = config.age.secrets."caddy.env".path;
